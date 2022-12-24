2cb3b6008288dc02b773eeeee18a8edcec9886be
geb789 <56951253+geb789@users.noreply.github.com>
2022-12-24 11:29:32 +0800

    Update legacy.rs
    cargo fmt

--- frame/ethereum/src/tests/legacy.rs
+++ frame/ethereum/src/tests/legacy.rs
@@ -23,7 +23,6 @@ use frame_support::{
 	dispatch::{DispatchClass, GetDispatchInfo},
 	weights::Weight,
 };
-use pallet_evm::AddressMapping;
 
 fn legacy_erc20_creation_unsigned_transaction() -> LegacyUnsignedTransaction {
 	LegacyUnsignedTransaction {
@@ -384,9 +383,6 @@ fn validated_transaction_apply_zero_gas_price_works() {
 	let (pairs, mut ext) = new_test_ext_with_initial_balance(2, 1_000);
 	let alice = &pairs[0];
 	let bob = &pairs[1];
-	let substrate_alice =
-		<Test as pallet_evm::Config>::AddressMapping::into_account_id(alice.address);
-	let substrate_bob = <Test as pallet_evm::Config>::AddressMapping::into_account_id(bob.address);
 
 	ext.execute_with(|| {
 		let transaction = LegacyUnsignedTransaction {
@@ -404,8 +400,8 @@ fn validated_transaction_apply_zero_gas_price_works() {
 			transaction
 		));
 		// Alice didn't pay fees, transfer 100 to Bob.
-		assert_eq!(Balances::free_balance(&substrate_alice), 900);
+		assert_eq!(Balances::free_balance(&alice.account_id), 900);
 		// Bob received 100 from Alice.
-		assert_eq!(Balances::free_balance(&substrate_bob), 1_100);
+		assert_eq!(Balances::free_balance(&bob.account_id), 1_100);
 	});
 }
